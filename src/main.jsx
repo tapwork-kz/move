4ff606ba93e899765d18066227e066232fff8228
Mechta <mechtatx@gmail.com>
2026-06-15 10:13:03 +0500

    Refactor imports and add CSS styles
    Added import for compiled styles and fixed formatting.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css'; // Строка подключения скомпилированных стилей
 
-// Находим div с id="root" и рендерим туда наше приложение
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
